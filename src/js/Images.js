8695c454c21f6da5ea3168ae973183bfe644c66b
joochahyana <joannachahyana@gmail.com>
2021-05-27 10:58:46 +0700

    move scripts from Assets.css to Main.css, delete Assets.css, and update Images.js

--- src/js/Images.js
+++ src/js/Images.js
@@ -1,7 +1,11 @@
 import imgHappy5Logo from '../images/Logo/Culture/Logo-Square.png';
 import imgPurplePlusIcon from '../images/icon/action/plus/plus - purple.png';
+import imgCloseIcon from '../images/icon/navigation/close.png';
+import imgDialogBase from '../images/Card/Base.png';
 
 export const Images = {
     "imgHappy5Logo": imgHappy5Logo,
-    "imgPurplePlusIcon": imgPurplePlusIcon
+    "imgPurplePlusIcon": imgPurplePlusIcon,
+    "imgCloseIcon": imgCloseIcon,
+    "imgDialogBase": imgDialogBase
 }
