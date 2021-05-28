c3c85c104a28802f79ac2aa486a4de927ecd09d6
joochahyana <joannachahyana@gmail.com>
2021-05-28 12:21:13 +0700

    updated create task dialog to also be used for edit task

--- src/js/DialogCreateEditTask.js
+++ src/js/DialogCreateEditTask.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Dialog.css';
 import { Images } from './Images.js';
 
-export const DialogCreateTask = (props) => {
+export const DialogCreateEditTask = (props) => {
+    const [headerText, setHeaderText] = useState("Create Task");
+    const [taskNameValue, setTaskNameValue] = useState("");
+    const [weightValue, setWeightValue] = useState("");
+    
+    useEffect(() => {
+        if (props.isCreateNewTask){ // create new task
+            setHeaderText("Create Task");
+            setTaskNameValue("");
+            setWeightValue("");
+        } else { // edit task
+            setHeaderText("Edit Task");
+            setTaskNameValue(props.taskName);
+            setWeightValue(props.weight);
+        }
+    }, [props.isCreateNewTask]);
+    
+    const handleChangeInputTaskName = ({target}) => {
+        // props.onChangeTaskName();
+        console.log(target.value);
+    }
+
     return (
         <div className="overlay">
             <div className="dialog-create-edit-task">
-                <img className="dialog-base"
-                    src={Images.imgDialogBase}
-                    alt="dialog-base"
-                    draggable="false" />
                 <header className="dialog-header">
-                    <h1 className="dialog-header-text">Create Task</h1>
+                    <h1 className="dialog-header-text">{headerText}</h1>
                     <img className="dialog-button-close clickable"
                         src={Images.imgCloseIcon}
                         alt="close-icon"
@@ -23,6 +40,7 @@ export const DialogCreateTask = (props) => {
                         <input className="dialog-input-task-name"
                             type="text"
                             placeholder="example: Build rocket to Mars."
+                            defaultValue={taskNameValue}
                             onChange={props.onChangeTaskName} />
                     </div>
                     <div className="dialog-content-weight">
@@ -30,6 +48,7 @@ export const DialogCreateTask = (props) => {
                         <input className="dialog-input-weight"
                             type="text"
                             placeholder="0%"
+                            defaultValue={weightValue}
                             onChange={props.onChangeWeight} />
                     </div>
                     <div className="dialog-buttons">
